d8321ae1b09d478d658f88869d239a700784db84
Bramandia Gilanglaksana Adam <gilangbram@gmail.com>
2021-08-13 21:09:12 +0700

    :sparkles: dashboard shell

--- pages/index.js
+++ pages/index.js
@@ -1,44 +1,43 @@
 import Head from 'next/head';
-import { Button, Heading, Text, Code } from '@chakra-ui/core';
+
+import { Button, Flex, Icon } from '@chakra-ui/core';
 import { useAuth } from '@/lib/auth';
 
 const Home = () => {
   const auth = useAuth();
-  return (
-    <div className="container">
-      <Head>
-        <title>Express Feedback</title>
-      </Head>
 
-      <main>
-        <h1 className="title">Express Feedback</h1>
-        <Text>
-          Current user:{' '}
-          <Code>
-            {auth.user
-              ? auth.user.email
-                ? auth.user.email
-                : auth.user.name
-              : 'None'}
-          </Code>
-        </Text>
-        {auth?.user ? (
-          <Button onClick={(e) => auth.signOut()}>Sign Out</Button>
+  return (
+    <>
+      <Flex
+        as="main"
+        direction="column"
+        align="center"
+        justify="center"
+        h="100vh"
+      >
+        <Head>
+          <title>Express Feedback</title>
+        </Head>
+        <Icon color="black" name="logo" size="64px" />
+        {auth.user ? (
+          <Button as="a" mt={4} size="sm" href="/dashboard">
+            View Dashboard
+          </Button>
         ) : (
           <>
-            <Button onClick={(e) => auth.signInWithGitHub()}>
-              GitHub Sign In
+            <Button mt={4} size="sm" onClick={(e) => auth.signInWithGitHub()}>
+              Github Sign In
             </Button>
-            <Button onClick={(e) => auth.signInWithGoogle()}>
+            <Button mt={4} size="sm" onClick={(e) => auth.signInWithGoogle()}>
               Google Sign In
             </Button>
-            <Button onClick={(e) => auth.signInWithTwitter()}>
+            <Button mt={4} size="sm" onClick={(e) => auth.signInWithTwitter()}>
               Twitter Sign In
             </Button>
           </>
         )}
-      </main>
-    </div>
+      </Flex>
+    </>
   );
 };
 
