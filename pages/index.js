ec7c8413dfe1b2ce897572967cce040e5c548aa5
Bramandia Gilanglaksana Adam <gilangbram@gmail.com>
2021-08-15 15:02:22 +0700

    :zap: update feedback page

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import Head from 'next/head';
-import { Button, Flex, Text, Icon, Link } from '@chakra-ui/core';
+import { Button, Flex, Text, Icon, Link, Stack, Box } from '@chakra-ui/core';
 
 import { useAuth } from '@/lib/auth';
 
@@ -7,69 +7,125 @@ const Home = () => {
   const auth = useAuth();
 
   return (
-    <Flex
-      as="main"
-      direction="column"
-      align="center"
-      justify="center"
-      h="100vh"
-      maxW="400px"
-      margin="0 auto"
-    >
-      <Head>
-        <title>Express Feedback</title>
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
+    <Box bg="gray.100">
+      <Flex
+        as="main"
+        direction="column"
+        align="center"
+        justify="center"
+        h="100vh"
+        maxW="400px"
+        margin="0 auto"
+      >
+        <Head>
+          <title>Express Feedback</title>
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
               if (document.cookie && document.cookie.includes('express-feedback-auth')) {
                 window.location.href = "/dashboard"
               }
               `
-          }}
-        />
-      </Head>
-      <Icon color="black" name="logo" size="42px" mb={2} />
-      <br />
-      <Text mb={4} textAlign="justify">
-        <Text as="span" fontWeight="bold" display="inline">
-          Express Feedback
+            }}
+          />
+        </Head>
+        <Icon color="black" name="logo" size="64px" mb={4} />
+        <Text mb={4} textAlign="justify" p={8}>
+          <Text as="span" fontWeight="bold" display="inline">
+            Express Feedback
+          </Text>
+          {' is built by '}
+          <Link
+            href="https://github.com/gilangadam"
+            isExternal
+            textDecoration="underline"
+          >
+            Gilang Adam
+          </Link>
+          {` with guidance from `}
+          <Link
+            href="https://react2025.com"
+            isExternal
+            textDecoration="underline"
+          >
+            React 2025
+          </Link>
+          {`. It's the easiest way to add feedback, comments, or reviews to your static site.`}
         </Text>
-        {' is built by '}
-        <Link
-          href="https://github.com/gilangadam"
-          isExternal
-          textDecoration="underline"
-        >
-          Gilang Adam
-        </Link>
-        {` with guidance from `}
-        <Link
-          href="https://react2025.com"
-          isExternal
-          textDecoration="underline"
-        >
-          React 2025
-        </Link>
-        {`. It's the easiest way to add feedback, comments, or reviews to your static site.`}
-      </Text>
-      {auth.user ? (
-        <Button as="a" mt={4} size="sm" href="/dashboard">
-          View Dashboard
-        </Button>
-      ) : (
-        <>
-          <Button mt={4} size="sm" onClick={(e) => auth.signInWithGitHub()}>
-            Github Sign In
+        {auth.user ? (
+          <Button
+            as="a"
+            backgroundColor="white"
+            color="gray.900"
+            variant="outline"
+            fontWeight="medium"
+            href="/dashboard"
+            mt={4}
+            size="lg"
+            _hover={{ bg: 'gray.100' }}
+            _active={{
+              bg: 'gray.100',
+              transform: 'scale(0.95)'
+            }}
+          >
+            View Dashboard
           </Button>
-          <Button mt={4} size="sm" onClick={(e) => auth.signInWithGoogle()}>
-            Google Sign In
-          </Button>
-          <Button mt={4} size="sm" onClick={(e) => auth.signInWithTwitter()}>
-            Twitter Sign In
-          </Button>
-        </>
-      )}
-    </Flex>
+        ) : (
+          <Stack>
+            <Button
+              onClick={(e) => auth.signInWithGitHub()}
+              backgroundColor="gray.900"
+              color="white"
+              fontWeight="medium"
+              leftIcon="github"
+              mt={4}
+              size="lg"
+              _hover={{ bg: 'gray.700' }}
+              _active={{
+                bg: 'gray.800',
+                transform: 'scale(0.95)'
+              }}
+            >
+              Sign In with GitHub
+            </Button>
+            <Button
+              onClick={(e) => auth.signInWithGoogle()}
+              backgroundColor="white"
+              color="gray.900"
+              variant="outline"
+              fontWeight="medium"
+              leftIcon="google"
+              mt={4}
+              size="lg"
+              _hover={{ bg: 'gray.100' }}
+              _active={{
+                bg: 'gray.100',
+                transform: 'scale(0.95)'
+              }}
+            >
+              Sign In with Google
+            </Button>
+            <Button
+              onClick={(e) => auth.signInWithTwitter()}
+              backgroundColor="white"
+              color="blue.400"
+              variant="outline"
+              fontWeight="medium"
+              leftIcon="twitter"
+              mt={4}
+              size="lg"
+              _hover={{ bg: 'blue.100' }}
+              _active={{
+                bg: 'white',
+                transform: 'scale(0.95)'
+              }}
+            >
+              Sign In with Twitter
+            </Button>
+          </Stack>
+        )}
+      </Flex>
+    </Box>
   );
 };
 
