376de28cdce04fcc8824070f4ab8d30ff4f3d6f3
Bramandia Gilanglaksana Adam <gilangbram@gmail.com>
2021-08-19 16:33:06 +0700

    :bug: fixing viewport meta tags

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,22 @@
+import Head from 'next/head';
 import { CSSReset, ThemeProvider } from '@chakra-ui/core';
 import { Global, css } from '@emotion/core';
 import { DefaultSeo } from 'next-seo';
 
 import { AuthProvider } from '@/lib/auth';
 import customTheme from '@/styles/theme';
+
 import SEO from 'next-seo.config';
 
 const GlobalStyle = ({ children }) => {
   return (
     <>
+      <Head>
+        <meta
+          name="viewport"
+          content="width=device-width, viewport-fit=cover, initial-scale=1"
+        />
+      </Head>
       <CSSReset />
       <Global
         styles={css`
--- pages/_document.js
+++ pages/_document.js
@@ -6,7 +6,6 @@ class MyDocument extends Document {
       <Html>
         <Head>
           <meta content="IE=edge" httpEquiv="X-UA-Compatible" />
-          <meta content="width=device-width, initial-scale=1" name="viewport" />
           <meta content="#ffffff" name="theme-color" />
           <meta content="#ffffff" name="msapplication-TileColor" />
           <meta
