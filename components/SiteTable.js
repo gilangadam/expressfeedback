2eef6706ecd491dc900658f3821c9592c37fd108
Bramandia Gilanglaksana Adam <gilangbram@gmail.com>
2021-08-30 07:59:58 +0700

    :zap: add footer, navbar, & other touches

--- components/SiteTable.js
+++ components/SiteTable.js
@@ -6,59 +6,49 @@ import { parseISO, format } from 'date-fns';
 import { Table, Tr, Th, Td } from './Table';
 import DeleteSiteButton from './DeleteSiteButton';
 
-const SiteTable = ({ sites }) => {
-  return (
-    <Box overflowX="scroll">
-      <Table w="full">
-        <thead>
-          <Tr>
-            <Th>Name</Th>
-            <Th>Site Link</Th>
-            <Th>Feedback Link</Th>
-            <Th>Date Added</Th>
-            <Th width="50px">{''}</Th>
-          </Tr>
-        </thead>
-        <tbody>
-          {sites.map((site, index) => (
-            <Box as="tr" key={site.id}>
-              <Td>
-                <NextLink
-                  href="/site/[siteId]"
-                  as={`/site/${site.id}`}
-                  passHref
-                >
-                  <Link id={`site-table-link-${index}`} fontWeight="medium">
-                    {site.name}
-                  </Link>
-                </NextLink>
-              </Td>
-              <Td>
-                <Link href={site.url} isExternal>
-                  {site.url}
+const SiteTable = ({ sites }) => (
+  <Box overflowX="scroll">
+    <Table w="full">
+      <thead>
+        <Tr>
+          <Th>Name</Th>
+          <Th>Site Link</Th>
+          <Th>Feedback Link</Th>
+          <Th>Date Added</Th>
+          <Th width="50px">{''}</Th>
+        </Tr>
+      </thead>
+      <tbody>
+        {sites.map((site, index) => (
+          <Box as="tr" key={site.id}>
+            <Td>
+              <NextLink href="/site/[siteId]" as={`/site/${site.id}`} passHref>
+                <Link id={`site-table-link-${index}`} fontWeight="medium">
+                  {site.name}
                 </Link>
-              </Td>
-              <Td>
-                <NextLink
-                  href="/site/[siteId]"
-                  as={`/site/${site.id}`}
-                  passHref
-                >
-                  <Link color="blue.500" fontWeight="medium">
-                    View Feedback
-                  </Link>
-                </NextLink>
-              </Td>
-              <Td>{format(parseISO(site.createdAt), 'PPpp')}</Td>
-              <Td>
-                <DeleteSiteButton index={index} siteId={site.id} />
-              </Td>
-            </Box>
-          ))}
-        </tbody>
-      </Table>
-    </Box>
-  );
-};
+              </NextLink>
+            </Td>
+            <Td>
+              <Link href={site.url} isExternal>
+                {site.url}
+              </Link>
+            </Td>
+            <Td>
+              <NextLink href="/site/[siteId]" as={`/site/${site.id}`} passHref>
+                <Link color="blue.500" fontWeight="medium">
+                  View Feedback
+                </Link>
+              </NextLink>
+            </Td>
+            <Td>{format(parseISO(site.createdAt), 'PPpp')}</Td>
+            <Td>
+              <DeleteSiteButton index={index} siteId={site.id} />
+            </Td>
+          </Box>
+        ))}
+      </tbody>
+    </Table>
+  </Box>
+);
 
 export default SiteTable;
